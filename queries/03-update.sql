a500517bd77e1a9db303694bf9c7085579860e65
Murilo <murilo.r.rocha@aluno.senai.br>
2025-09-22 14:40:41 -0300

    Feat: cria comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,15 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
+
+SELECT * FROM usuarios;

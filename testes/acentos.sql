efbbfc649d93f811df8fc9cc23ad153c929e9924
Murilo <murilo.r.rocha@aluno.senai.br>
2025-09-29 11:42:06 -0300

    Cria pasta testes

---
+++ testes/acentos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes (nome) VALUES
+('João'),
+('Stéfanny'),
+('Ângelo'),
+('Márcia'),
+('Letícia'),
+('Júlia'),
+('Vinícius');
